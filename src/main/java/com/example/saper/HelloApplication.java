acf1f7e2183a89975282b89f9c70f7ac8f412d92
Mariusz Janyszek <235881@edu.p.lodz.pl>
2022-05-11 12:56:09 +0200

    Dodanie przyciskow

--- src/main/java/com/example/saper/HelloApplication.java
+++ src/main/java/com/example/saper/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.saper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        GridPane grid=new GridPane();
+        Button[][] buttons =new Button[8][8];
+        for (int i=0;i<8;i++)
+        {
+            for (int j=0;j<8;j++){
+                buttons[i][j]=new Button();
+                grid.add(buttons[i][j],i,j,1,1);
+            }
+        }
+
+
+        Scene scene=new Scene(grid);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
