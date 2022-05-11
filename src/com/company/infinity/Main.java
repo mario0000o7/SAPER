cc2711ec6919e5288986d18a35bbe99f01947ff9
Mariusz Janyszek <235881@edu.p.lodz.pl>
2022-05-11 12:50:25 +0200

    Dodanie javafx

--- src/com/company/infinity/Main.java
+++ src/com/company/infinity/Main.java
@@ -1,8 +1,16 @@
 package com.company.infinity;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Stage stage = new Stage();
     }
 }
